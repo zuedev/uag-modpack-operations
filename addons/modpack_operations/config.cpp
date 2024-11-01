b9e2e387ce7bdeedccb6d5fff0a1bb99291c3d12
zuedev <zuedev@gmail.com>
2024-11-01 01:03:49 +0000

    initial commit

---
+++ addons/modpack_operations/config.cpp
@@ -0,0 +1,11 @@
+class CfgPatches {
+    class modpack_operations {
+        authors[] = {"zuedev"};
+        authorUrl = "https://uagpmc.com";
+        name = "modpack_operations";
+        requiredAddons[] = {};
+        requiredVersion = 2.16;
+        units[] = {};
+        weapons[] = {};
+    };
+};
